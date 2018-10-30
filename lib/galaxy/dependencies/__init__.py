855936e6987b84407f77db1ec5a2ff9d77410739
mvdbeek <m.vandenbeek@gmail.com>
2018-10-30 16:28:28 +0100

    Make pykube a conditional requirement

--- lib/galaxy/dependencies/__init__.py
+++ lib/galaxy/dependencies/__init__.py
@@ -94,6 +94,9 @@ class ConditionalDependencies(object):
     def check_pbs_python(self):
         return "galaxy.jobs.runners.pbs:PBSJobRunner" in self.job_runners
 
+    def check_pykube(self):
+        return "galaxy.jobs.runners.kubernetes:KubernetesJobRunner" in self.job_runners
+
     def check_python_openid(self):
         return asbool(self.config["enable_openid"])
 
